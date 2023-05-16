d79e20f06decd3dee6d221f7be50430de868dcb0
LDER <ludjy.dev@gmail.com>
2023-05-16 19:44:02 -0400

    added play feature for profile

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
-let editButton = document.querySelector('.editButton')
 
+const editButton = document.querySelector('.editButton')
+const player = document.querySelector('.player-container')
+const progressContainer = document.querySelector('.progressContainer')
+const progressBar = document.querySelector('.progBar')
+const audio = document.querySelector('#audio')
 
+const backBtn = document.querySelector('#prev')
+const playBtn = document.querySelector('#play')
+const forwardBtn = document.querySelector('#next')
 
 
 
@@ -10,7 +17,48 @@ function editProfile() {
   modalObj.show();
 }
 
+function playSong(){
+player.classList.add('play')
+playBtn.querySelector('i').classList.remove('fa-play')
+playBtn.querySelector('i').classList.add('fa-pause')
+audio.play()
+}
 
+function pauseSong(){
+  player.classList.remove('play')
+  playBtn.querySelector('i').classList.add('fa-play')
+playBtn.querySelector('i').classList.remove('fa-pause')
+audio.pause()
+}
 
+function back(){
+
+}
+
+function forward(){
+
+}
+
+function updateProgress(e){
+const {duration, currentTime} = e.srcElement
+const progressPercent = (currentTime/ duration)*100
+progressBar.style.width = `${progressPercent}%`
+
+}
 
 editButton.addEventListener('click', editProfile)
+//play song
+playBtn.addEventListener('click', () => {
+  const isPlaying = player.classList.contains('play')
+  if(isPlaying){
+    pauseSong()
+  }else{
+    playSong()
+  }
+})
+
+backBtn.addEventListener('click', back)
+
+forwardBtn.addEventListener('click' ,forward)
+
+audio.addEventListener('timeupdate', updateProgress)
