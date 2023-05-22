a3652e81b05d6837adeeeee2bba5d29a2d8cff32
LDER <ludjy.dev@gmail.com>
2023-05-22 10:17:15 -0400

    update to radio

--- public/radio.js
+++ public/radio.js
@@ -3,7 +3,7 @@ import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color
 import { OrbitControls } from 'https://unpkg.com/three@0.146/examples/jsm/controls/OrbitControls.js';
 import { createSculptureWithGeometry } from 'https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js';
 import { spCode } from '../shaderPark.js';
-let audioSource = document.querySelector('audio').src
+let audioSource = document.getElementById('audio').src
 let scene = new Scene();
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -111,57 +111,3 @@ let render = () => {
 };
 
 render();
-// audio player
-const players = document.getElementsByClassName('player-container');
-const progressContainer = document.querySelector('.progressContainer');
-const progressBar = document.querySelector('.progBar');
-const audios = document.getElementsByClassName('audio');
-const backBtn = document.querySelector('#prev');
-const playBtn = document.getElementsByClassName('playBtn');
-const forwardBtn = document.querySelector('#next');
-const AudioArray = Array.from(audios);
-const playerArray = Array.from(players);
-
-
-function back() {
-  // Implementation for back function
-}
-
-function forward() {
-  // Implementation for forward function
-}
-
-function updateProgress(e) {
-  const {duration, currentTime} = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  progressBar.style.width = `${progressPercent}%`;
-}
-
-// for each player, play button and audio element
-for(let i = 0; i < playerArray.length; i++) {
-  let player = playerArray[i];
-  let playButton = playBtn[i];
-  let audio = AudioArray[i];
-
-  
-  playButton.addEventListener('click', () => {
-    const isPlaying = player.classList.contains('play');
-    if (isPlaying) {
-      player.classList.remove('play');
-      playButton.querySelector('i').classList.add('fa-play');
-      playButton.querySelector('i').classList.remove('fa-pause');
-      audio.pause();
-    } else {
-      player.classList.add('play');
-      playButton.querySelector('i').classList.remove('fa-play');
-      playButton.querySelector('i').classList.add('fa-pause');
-      audio.play();
-    }
-  });
-
-  // add progress event for each audio
-  audio.addEventListener('timeupdate', updateProgress);
-}
-
-backBtn.addEventListener('click', back);
-forwardBtn.addEventListener('click', forward)
