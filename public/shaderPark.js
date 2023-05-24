6a800071866585bc7d945f14f0ece2762eb3d9e3
LDER <ludjy.dev@gmail.com>
2023-05-24 13:16:07 -0400

    radio functional w/o audio reactivity

--- public/shaderPark.js
+++ public/shaderPark.js
@@ -8,13 +8,13 @@ export function spCode() {
     let s = getSpace();
     let r = getRayDirection();
     
-    let n1 = noise(r * 4 +vec3(0, audio, vec3(0, audio, audio))*.5 );
-    let n = noise(s + vec3(0, 0, audio+time*.1) + n1);
+    let n1 = noise(r * 4 +vec3(0, time, vec3(0, time, time))*.5 );
+    let n = noise(s + vec3(0, 0, time*.5) + n1);
     
     metal(n*.5+.5);
     shine(n*.5+.5);
     
-    color(normal * .1 + vec3(0, 0, 1));
+    color(normal * .1 + vec3(1, 0.8, 0.0));
     displace(mouse.x * 2, mouse.y * 2, 0);
     boxFrame(vec3(2), abs(n) * .1 + .04 );
     mixGeo(pointerDown);
--- public/singlePlayer.js
+++ public/singlePlayer.js
@@ -1,86 +1,97 @@
 
-const editButton = document.querySelector('.editButton')
 const player = document.querySelector('.radio-container')
 const progressContainer = document.querySelector('.progressContainer')
 const progressBar = document.querySelector('.progBar')
 const audio = document.querySelector('#audio')
-const banner = document.querySelector('.profileBanner')
 const backBtn = document.querySelector('#prev')
 const playBtn = document.querySelector('#play')
 const forwardBtn = document.querySelector('#next')
+const trackTitle = document.querySelector('.trackTitle')
+const aritst = document.querySelector('.artist')
 
 
 
-function editProfile() {
-  const profileModal = document.getElementById('profileModal');
-  const modalObj = new bootstrap.Modal(profileModal);
-  modalObj.show();
+
+
+function playSong() {
+  player.classList.add('play')
+  playBtn.querySelector('i').classList.remove('fa-play')
+  playBtn.querySelector('i').classList.add('fa-pause')
+  audio.play()
 }
 
-// function editBanner() {
-//   const bannerModal = document.getElementById('bannerModal');
-//   const modalObj = new bootstrap.Modal(bannerModal);
-//   modalObj.show();
-// }
-
-function playSong(){
-player.classList.add('play')
-playBtn.querySelector('i').classList.remove('fa-play')
-playBtn.querySelector('i').classList.add('fa-pause')
-audio.play()
-}
-
-function pauseSong(){
+function pauseSong() {
   player.classList.remove('play')
   playBtn.querySelector('i').classList.add('fa-play')
-playBtn.querySelector('i').classList.remove('fa-pause')
-audio.pause()
+  playBtn.querySelector('i').classList.remove('fa-pause')
+  audio.pause()
 }
 
-function back(){
-
-}
-
-function forward(){
-
-}
-
-function updateProgress(e){
-const {duration, currentTime} = e.srcElement
-const progressPercent = (currentTime/ duration)*100
-progressBar.style.width = `${progressPercent}%`
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progressBar.style.width = `${progressPercent}%`
 
 }
 //play song
 playBtn.addEventListener('click', () => {
   const isPlaying = player.classList.contains('play')
-  if(isPlaying){
+  if (isPlaying) {
     pauseSong()
-  }else{
+  } else {
     playSong()
   }
 })
 
-backBtn.addEventListener('click', back)
 
-forwardBtn.addEventListener('click' ,forward)
 
 audio.addEventListener('timeupdate', updateProgress)
 
 // Fetch the songs from the server
-function trackFetch(){
-  fetch('/tracks')
+
+fetch('/tracks')
   .then((response) => response.json())
   .then((data) => {
-    const songUrls = data.map((track) => track.song);
+    const trackUrls = data.map((track) => track.song);
     const trackNames = data.map((track) => track.trackName);
     const uploadedBy = data.map((track) => track.uploadedBy);
- 
-    console.log(songUrls);
+
+    console.log(trackUrls);
     console.log(trackNames);
     console.log(uploadedBy);
+    let trackIndex = 3
+    trackLoad(trackIndex)
+
+    function trackLoad(trackIndex) {
+      trackTitle.innerText = trackNames[trackIndex]
+      aritst.innerText = uploadedBy[trackIndex]
+      audio.src = trackUrls[trackIndex]
+
+    }
+
+    function back() {
+      trackIndex--
+      if(trackIndex < 0){
+        trackIndex = trackUrls.length-1
+      }
+      trackLoad(trackIndex)
+      playSong()
+    }
+
+    function forward() {
+      trackIndex++
+      if(trackIndex > trackUrls.length-1){
+        trackIndex = 0
+      }
+      trackLoad(trackIndex)
+      playSong()
+    }
+    backBtn.addEventListener('click', back)
+
+    forwardBtn.addEventListener('click', forward)
   })
   .catch((error) => console.error(error));
-}
 
-trackFetch()
+
+
+  
